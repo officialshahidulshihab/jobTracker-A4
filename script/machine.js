b1f3b3624c0783a294af11cca7dff926c5c199d2
officialshahidulshihab <officialshahidulshihab@gmail.com>
2026-02-23 16:40:05 +0600

    added js machine code

---
+++ script/machine.js
@@ -0,0 +1,11 @@
+function getAnyElementById(id){
+    const elementId =document.getElementById(id)
+    return elementId;
+
+}
+function getAnyElementByClassName(classList){
+    const elementClass =document.getElementsByClassName(classList)
+    return elementClass;
+
+}
+
